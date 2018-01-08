6f85a16fd508b80d51ba1e841af706fe7324614c
RobertPoncelet <robert.poncelet@talk21.com>
2018-01-08 18:14:28 +0000

    New Activity to receive JSON intents from Qubism, but it doesn't work yet

---
+++ src/main/java/test/zhlt_android/JSONActivity.java
@@ -0,0 +1,19 @@
+package test.zhlt_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class JSONActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    }
+}
