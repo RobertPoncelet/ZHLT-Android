66fe60a109bf129a9eeba83d1671a676c0a138cf
RobertPoncelet <robert.poncelet@talk21.com>
2018-01-31 17:57:42 +0000

    More progress on parsing JSON files

--- src/main/java/test/zhlt_android/JsonParser.java
+++ src/main/java/test/zhlt_android/JsonParser.java
@@ -4,10 +4,34 @@ package test.zhlt_android;
  * Created by robert.poncelet on 29/01/18.
  */
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+
 import test.zhlt_android.FileUtils;
 import test.zhlt_android.MapFile;
 
 public class JsonParser {
+    public void parse(File jsonFile) {
+        try {
 
+            String jsonString = FileUtils.getStringFromFile(jsonFile.getPath());
+            JSONObject json = new JSONObject(jsonString);
+            JSONArray qubes = json.getJSONArray("qubes");
+
+            MapFile map = new MapFile();
+            map.ents.add(new MapFile.Entity("worldspawn"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MapFile.Brush parseQube(JSONObject qube) throws JSONException {
+        qube.getDouble("pos_x");
+    }
 }
--- src/main/java/test/zhlt_android/MapFile.java
+++ src/main/java/test/zhlt_android/MapFile.java
@@ -1,5 +1,8 @@
 package test.zhlt_android;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -7,40 +10,127 @@ import java.util.Map;
  */
 
 public class MapFile {
-    private Entity[] ents;
+    public List<Entity> ents;
 
-    public class Entity {
+    public static class Entity {
         public Map<String, String> kvs;
         public Brush[] brushes;
 
         public Entity(String classname) {
             kvs.put("classname", classname);
         }
+
+        private void out(PrintStream p) {
+            p.print("{\n");
+            for (Map.Entry<String, String> kv : kvs.entrySet())
+            {
+                p.print("\t\"");
+                p.print(kv.getKey());
+                p.print("\" \"");
+                p.print(kv.getValue());
+                p.print("\"\n");
+            }
+            for (Brush b : brushes)
+            {
+                b.out(p);
+            }
+        }
     }
 
-    public class Brush {
-        public Face[] faces;
+    public static class Brush {
+        public List<Face> faces;
+
+        public Brush(List<Face> inFaces) {
+            faces = inFaces;
+        }
+
+        private void out(PrintStream p) {
+            p.print("\t{\n");
+            for (Face f : faces) {
+                f.out(p);
+            }
+            p.print("\t}\n");
+        }
     }
 
-    public class Face {
+    public static class Face {
         public Vector points[];
         public String texName;
-        public Vector uAxis[];
-        public Vector vAxis[];
+        public Vector uAxis;
+        public Vector vAxis;
         public float rotation;
         public float scaleX;
         public float scaleY;
+
+        private void out(PrintStream p) {
+            p.print("\t\t");
+            for (Vector v : points) {
+                p.print("( ");
+                v.out(p);
+                p.print(" ) ");
+            }
+            p.print(texName);
+            p.print(" [ ");
+            uAxis.out(p);
+            p.print(" ] [ ");
+            vAxis.out(p);
+            p.print(" ] ");
+            p.print(String.valueOf(rotation));
+            p.print(" ");
+            p.print(String.valueOf(scaleX));
+            p.print(" ");
+            p.print(String.valueOf(scaleY));
+            p.print("\n");
+        }
     }
 
-    public class Vector {
-        private float p[];
+    public static class Vector {
+        private float v[];
 
         public Vector(float x, float y, float z) {
-            p = new float[] {x, y, z};
+            v = new float[] {x, y, z};
+        }
+        public Vector(float x, float y, float z, float w) {
+            v = new float[] {x, y, z, w};
+        }
+        public Vector(Vector other) {
+            v = new float[other.v.length];
+            for (int i = 0; i < other.v.length; ++i) {
+                v[i] = other.v[i];
+            }
         }
 
-        public Vector(float x, float y, float z, float w) {
-            p = new float[] {x, y, z, w};
+        private void out(PrintStream p) {
+            for (int i = 0; i < v.length; ++i) {
+                p.print(String.valueOf(v[i]));
+                if (i != v.length - 1) {
+                    p.print(" ");
+                }
+            }
+        }
+
+        public Vector plus(Vector b) {
+            Vector a = new Vector(this);
+            int length = a.v.length < b.v.length ? a.v.length : b.v.length;
+            for (int i = 0; i < length; ++i) {
+                a.v[i] += b.v[i];
+            }
+            return a;
+        }
+
+        public Vector minus(Vector b) {
+            Vector a = new Vector(this);
+            int length = a.v.length < b.v.length ? a.v.length : b.v.length;
+            for (int i = 0; i < length; ++i) {
+                a.v[i] -= b.v[i];
+            }
+            return a;
+        }
+    }
+
+    public void write(PrintStream p) {
+        for (Entity ent : ents) {
+            ent.out(p);
         }
     }
 }
